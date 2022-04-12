aedfafa300c1d246d67fd6ab0cbde3d7c3e17084
Ibrahim El Gohary <goharyibrahim@gmail.com>
2022-04-13 00:03:11 +0200

    cors

--- pages/api/api-insert-test-mongo-check-first.js
+++ pages/api/api-insert-test-mongo-check-first.js
@@ -1,7 +1,19 @@
 import { connectToDatabase } from "../../lib/db";
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
 
 async function handler(req, res) {
   if (req.method === "POST") {
+    await cors(req, res);
     const client = await connectToDatabase();
     const db = client.db();
     const testsList = req.body;
