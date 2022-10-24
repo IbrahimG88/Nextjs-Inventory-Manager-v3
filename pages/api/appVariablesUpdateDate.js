960134fe58932cba93c84c4939ab76eff82f4c85
Ibrahim El Gohary <goharyibrahim@gmail.com>
2022-10-24 19:04:08 +0200

    Update appVariablesUpdateDate.js

--- pages/api/appVariablesUpdateDate.js
+++ pages/api/appVariablesUpdateDate.js
@@ -18,9 +18,11 @@ export default async (req, res) => {
 
   // const date = new Date().toLocaleString();
 
-  const date = new Date();
-  date.setHours(date.getHours() + 2);
-  const finalDate = date.toLocaleString();
+  //const date = new Date();
+ // date.setHours(date.getHours() + 2);
+  //const finalDate = date.toLocaleString();
+  
+   const date = new Date().toLocaleString();
 
   const client = await connectToDatabase();
   const db = client.db();
@@ -28,7 +30,7 @@ export default async (req, res) => {
     .collection("appVariables")
     .updateOne(
       { variableType: "date" },
-      { $set: { date: finalDate } },
+      { $set: { date: date } },
       { upsert: true }
     );
   return res.json(item);
