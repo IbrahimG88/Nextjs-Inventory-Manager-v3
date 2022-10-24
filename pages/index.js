feda50048e837996ce85a3708ac81b383392e05a
Ibrahim El Gohary <goharyibrahim@gmail.com>
2022-10-24 21:09:44 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,154 +1,41 @@
-import { Fragment } from "react";
+/* eslint-disable import/no-anonymous-default-export */
+import { connectToDatabase } from "../../lib/db";
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
 
-import { useState } from "react";
-
-import { Text } from "@chakra-ui/react";
-import Link from "next/link";
-
-export const getServerSideProps = async () => {
-  const panelTypes = [];
-  let numberOfRegisteredPatients;
-  const getPreviousDate = await fetch(
-    `${process.env.APP_URL}/api/appVariablesGetDate`
-  ).then((response) => {
-    return response.json().then((data) => {
-      // console.log("data look", data);
-
-      const myDate = new Date(data[0].date);
-
-      const testDateObject = {
-        day: myDate.getDate(),
-        month: myDate.getMonth() + 1,
-        year: myDate.getFullYear(),
-        hours: myDate.getHours(), 
-        minute: myDate.getMinutes(),
-      };
-      
-      console.log("testdateObject", testDateObject);
-      return myDate;
-    });
-  });
-  // console.log("getPreviousDate", new Date(getPreviousDate));
-
-  const dateIndividualData = (singleDate) => {
-    const dateObject = {
-      day: singleDate.getDate(),
-      month: singleDate.getMonth() + 1,
-      year: singleDate.getFullYear(),
-      hours: singleDate.getHours(),
-      minute: singleDate.getMinutes(),
-    };
-    console.log("dateObject", dateObject);
-    return dateObject;
-  };
-
-  const nowDate = () => {
-    const now = new Date();
-    const dateObject = {
-      day: now.getDate(),
-      month: now.getMonth() + 1,
-      year: now.getFullYear(),
-      hours: now.getHours(),
-      minute: now.getMinutes(),
-    };
-    console.log("now date", dateObject);
-    return dateObject;
-  };
-
-  const dateSample = dateIndividualData(new Date(getPreviousDate));
-  console.log("dateSample", dateSample);
-
-  const res = await fetch(
-    `http://197.45.107.206/api2/integration/worklist/${dateSample.year}-${
-      dateSample.month
-    }-${dateSample.day}%20${dateSample.hours}:${dateSample.minute}:00:00/${
-      nowDate().year
-    }-${nowDate().month}-${nowDate().day}%20${nowDate().hours}:${
-      nowDate().minute
-    }:00:00`,
-    { mode: "cors" }
-  ).then((response) => {
-    return response.json().then((data) => {
-      console.log("data", data);
-      console.log("nowDate function call", nowDate().minute);
-      return data;
-    });
-  });
-  console.log("res.length for number of patients", res.length);
-  numberOfRegisteredPatients = res.length;
-  for (const key in res) {
-    for (const myItem in res[key].panels) {
-      panelTypes.push(res[key].panels[myItem].report_name);
-    }
-  }
-
-  const custFreq = panelTypes.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] ?? 0) + 1;
-
-    return acc;
-  }, {});
-
-  const finalArray = [];
-
-  for (const key in custFreq) {
-    finalArray.push({
-      name: key,
-      frequency: custFreq[key],
-    });
-  }
-  if (res.length > 0) {
-    finalArray.push({
-      name: "Syringes",
-      frequency: res.length,
-    });
-  }
-  console.log("finalArray look", finalArray);
-
-  fetch(`${process.env.APP_URL}/api/optimizedUpdateItemStocks`, {
-    method: "POST",
-    body: JSON.stringify(finalArray),
-    headers: {
-      "content-Type": "application/json",
-    },
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
   })
-    .then((data) => data.json())
-    .then((data) => console.log("data here", data));
+);
 
-  // console.log("revalidate");
+//working
+export default async (req, res) => {
+  await cors(req, res);
 
-  await fetch(`${process.env.APP_URL}/api/appVariablesUpdateDate`);
+  // const date = new Date().toLocaleString();
 
-  return {
-    props: { finalArray },
-  };
+  const date = new Date();
+  //date.setHours(date.getHours() + 2);
+  //const finalDate = date.toLocaleString();
+
+  const client = await connectToDatabase();
+  const db = client.db();
+  const item = await db
+    .collection("appVariables")
+    .updateOne(
+      { variableType: "date" },
+      { $set: { date: date } },
+      { upsert: true }
+    );
+  return res.json(item);
 };
 
-export default function FrequencyWorklist({ finalArray }) {
-  console.log("finalArray", finalArray);
-  return (
-    <Text style={{ marginLeft: "50px" }}>
-      <br />
-      <br />
-      Welcome to the <strong>Inventory Manager app</strong> that automates
-      inventory consumption data. You can search all test items and add the
-      corresponding stocks in the
-      <Link href="/accordion-updated" passHref>
-        <strong> add stocks </strong>
-      </Link>
-      section. You can view all tests inventory and amounts remaining for each
-      item in the
-      <Link href="/react-table" passHref>
-        <a>
-          <strong> Inventory </strong>
-        </a>
-      </Link>
-      module.
-      <br />
-      <br />
-      In the Inventory you can seacrh for items and click the column title to
-      enable sorting.
-    </Text>
-  );
-}
+//sets a new Date put it down at the end
 
-// remmeber if you reset the syringes from database hardtype it in the database
+// time differs from deployment due to the 2 hours difference
+
+// the app is 2 hours later than the real  local time
